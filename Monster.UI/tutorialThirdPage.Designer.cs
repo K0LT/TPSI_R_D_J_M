38fc3b386968cd085ed7d247bec92150523963dd
marciaL14 <Marcia.Leite.T0130784@edu.atec.pt>
2025-05-15 20:23:39 +0100

    Implementaçao de tutoriais e pop up em newMonster

--- Monster.UI/tutorialThirdPage.Designer.cs
+++ Monster.UI/tutorialThirdPage.Designer.cs
@@ -29,18 +29,47 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tutorialThirdPage));
+            button_Tutorial1_Next = new Button();
+            pictureBox_Tutorial1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox_Tutorial1).BeginInit();
             SuspendLayout();
+            // 
+            // button_Tutorial1_Next
+            // 
+            button_Tutorial1_Next.FlatStyle = FlatStyle.Flat;
+            button_Tutorial1_Next.Image = (Image)resources.GetObject("button_Tutorial1_Next.Image");
+            button_Tutorial1_Next.Location = new Point(255, 808);
+            button_Tutorial1_Next.Name = "button_Tutorial1_Next";
+            button_Tutorial1_Next.Size = new Size(106, 40);
+            button_Tutorial1_Next.TabIndex = 26;
+            button_Tutorial1_Next.UseVisualStyleBackColor = true;
+            button_Tutorial1_Next.Click += button_Tutorial1_Next_Click;
+            // 
+            // pictureBox_Tutorial1
+            // 
+            pictureBox_Tutorial1.BackColor = Color.Transparent;
+            pictureBox_Tutorial1.Location = new Point(55, 36);
+            pictureBox_Tutorial1.Name = "pictureBox_Tutorial1";
+            pictureBox_Tutorial1.Size = new Size(500, 750);
+            pictureBox_Tutorial1.TabIndex = 25;
+            pictureBox_Tutorial1.TabStop = false;
             // 
             // tutorialThirdPage
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            Controls.Add(button_Tutorial1_Next);
+            Controls.Add(pictureBox_Tutorial1);
             Name = "tutorialThirdPage";
             Size = new Size(600, 900);
+            ((System.ComponentModel.ISupportInitialize)pictureBox_Tutorial1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button_Tutorial1_Next;
+        private PictureBox pictureBox_Tutorial1;
     }
 }
